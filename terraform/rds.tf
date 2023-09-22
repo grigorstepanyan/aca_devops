2b60ad5500937b59ec10eb51f5c77066ae4cb4c4
Your Name <you@example.com>
2023-09-22 21:46:19 +0400

    rds working

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,12 +1,32 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  username             = "root"
-  password             = "root1234321"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
+resource "aws_db_instance" "mysql_db" {
+  allocated_storage       = 10
+  db_name                 = "mydb"
+  engine                  = "mysql"
+  engine_version          = "5.7"
+  instance_class          = "db.t2.micro"
+  username                = "root"
+  password                = "root1234321"
+  parameter_group_name    = "default.mysql5.7"
+  vpc_security_group_ids  = [aws_security_group.rds_sg1.id]
+  skip_final_snapshot     = true
 }
+resource "aws_security_group" "rds_sg1" {
+  name        = "rds_sg1"
+  description = "Allow sql traffic"
 
+  ingress {
+    description      = "MySQL"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["172.16.0.0/12"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
