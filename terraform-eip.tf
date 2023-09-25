4e14239d319b4df38b5fe3f7873e4d556f12d85b
Grigor Stepanyan <grigor.stepanyan.1994@mail.ru>
2023-09-25 18:27:59 +0000

    eip

--- terraform-eip.tf
+++ terraform-eip.tf
@@ -7,9 +7,3 @@ resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.web_aca.id
   allocation_id = aws_eip.aca_eip.id
 }
-
-
-output "public_ip" {
-  value = aws_eip.aca_eip.public_ip
-  }
-
